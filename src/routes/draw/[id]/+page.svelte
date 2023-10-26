f1037a278db5c82449a063c166ad85915f8d3c38
willbraun <williamhbraun1@gmail.com>
2023-10-26 08:50:11 -0400

    starting to create bracket

--- src/routes/draw/[id]/+page.svelte
+++ src/routes/draw/[id]/+page.svelte
@@ -7,7 +7,8 @@
 	const title = `${data.draw.name} ${data.draw.event} ${data.draw.year}`
 </script>
 
-<header class="flex justify-end gap-2 p-2">
+<header class="flex justify-end items-center gap-2 p-2">
+	<p class="flex-grow text-xl font-bold ml-4">Tennis Bracket</p>
 	{#if pb.authStore.isValid}
 		<a data-sveltekit-reload href="/">
 			<button
@@ -32,6 +33,46 @@
 	{/if}
 </header>
 
-<h1>{title}</h1>
+<h1 class="text-2xl">{title}</h1>
 
-<p>{JSON.stringify(data.slots.items)}</p>
+<div class="grid grid-cols-5">
+	<!-- Header Row -->
+	<div class="border text-center">Round of 16</div>
+	<div class="border text-center">Quarterfinals</div>
+	<div class="border text-center">Semifinals</div>
+	<div class="border text-center">Final</div>
+	<div class="border text-center">Champion</div>
+
+	<!-- Round 1 -->
+	<div class="">
+		<div class="border-b-2 border-black text-center">
+			<div class="pt-2">Team A</div>
+		</div>
+		<div class="border-b-2 border-r-2 border-black text-center">
+			<div class="pt-2">Team B</div>
+		</div>
+		<div class="border-b-2 border-black text-center">
+			<div class="pt-2">Team C</div>
+		</div>
+		<div class="border-b-2 border-r-2 border-black text-center">
+			<div class="pt-2">Team D</div>
+		</div>
+	</div>
+
+	<!-- Round 2 -->
+	<div class="">
+		<div class="border-b-2 border-black text-center">
+			<div class="pt-6">Winner 1</div>
+		</div>
+		<div class="border-b-2 border-r-2 border-black text-center">
+			<div class="pt-11">Winner 2</div>
+		</div>
+	</div>
+
+	<!-- Round 3 (Final) -->
+	<div class="">
+		<div class="border-b-2 border-black text-center">
+			<div class="pt-16">Champion</div>
+		</div>
+	</div>
+</div>
