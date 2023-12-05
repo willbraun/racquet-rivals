36d2953222d7173e0dd5994b6cde356c35534e9b
willbraun <williamhbraun1@gmail.com>
2023-12-05 08:59:20 -0500

    starting refactor of select users for auth rules

--- src/routes/draw/[slug]/SelectUsers.svelte
+++ src/routes/draw/[slug]/SelectUsers.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
+	import Pocketbase, { ClientResponseError } from 'pocketbase'
 	import type { SvelteComponent } from 'svelte'
 	import { getModalStore } from '@skeletonlabs/skeleton'
 	import type { DeselectUserResult, SelectUserResult, SelectedUser } from '../../../lib/types'
 	import { applyAction, enhance } from '$app/forms'
 	import type { ActionResult } from '@sveltejs/kit'
 	import FormError from '../../../lib/FormError.svelte'
-	import { mainColor } from '../../../lib/utils'
+	import { mainColor, selectColors, errorMessage } from '../../../lib/utils'
+	import Cookies from 'js-cookie'
 	export let parent: SvelteComponent
 
+	const pb = new Pocketbase('https://tennisbracket.willbraun.dev')
 	const modalStore = getModalStore()
 
 	let value = ''
@@ -16,6 +19,11 @@
 	let error = ''
 	let deletedUserId = ''
 	let users: SelectedUser[] = $modalStore[0].meta.selectedUsers
+
+	// Base Classes
+	const cBase = 'card p-4 w-modal shadow-xl space-y-4'
+	const cHeader = 'text-2xl font-bold'
+	const cForm = 'rounded-container-token'
 
 	let inputRef: HTMLInputElement
 	const refocus = () => {
@@ -30,17 +38,93 @@
 		return result as DeselectUserResult
 	}
 
-	// Base Classes
-	const cBase = 'card p-4 w-modal shadow-xl space-y-4'
-	const cHeader = 'text-2xl font-bold'
-	const cForm = 'rounded-container-token'
+	const selectUser = async (username: string) => {
+		// const currentUser: SelectedUser = JSON.parse(cookies.get('currentUser') ?? '{}')
+		// const selectedUsers: SelectedUser[] = JSON.parse(
+		// 	cookies.get(`selectedUsers-${currentUser.id}`) ?? '[]'
+		// )
+
+		selectLoading = true
+
+		if (username === '') {
+			error = 'Please enter a username'
+			selectLoading = false
+			return
+		}
+
+		const currentUser: SelectedUser = JSON.parse(Cookies.get('currentUser') ?? '{}')
+
+		if (!currentUser.id) {
+			error = 'Must be logged in to select a user'
+			selectLoading = false
+			return
+		}
+
+		const selectedUsers: SelectedUser[] = JSON.parse(
+			Cookies.get(`selectedUsers-${currentUser.id}`) ?? '[]'
+		)
+		const allUsernames = [currentUser.username, ...selectedUsers.map((user) => user.username)]
+
+		if (allUsernames.length >= 6) {
+			error = 'Exceeds max of 6 total'
+			selectLoading = false
+			return
+		}
+
+		if (allUsernames.includes(username)) {
+			error = `User "${username}" is already selected`
+			selectLoading = false
+			return
+		}
+
+		const usedColors = selectedUsers.map((user) => user.color)
+		const availableColors = selectColors.filter((color) => !usedColors.includes(color))
+
+		try {
+			const data = await pb.collection('user').getFirstListItem(`username="${username}"`)
+			const newUser: SelectedUser = {
+				id: data.id,
+				username: data.username,
+				color: availableColors[0]
+			}
+			selectedUsers.push(newUser)
+			Cookies.set(`selectedUsers-${currentUser.id}`, JSON.stringify(selectedUsers), { expires: 7 })
+			users = [...users, newUser]
+			value = ''
+			error = ''
+			// cookies.set(`selectedUsers-${currentUser.id}`, JSON.stringify(selectedUsers), {
+			// 	maxAge: 60 * 60 * 24 * 400
+			// })
+			// return {
+			// 	user: {
+			// 		id: data.id,
+			// 		username: data.username,
+			// 		color: availableColors[0]
+			// 	} as SelectedUser,
+			// 	error: ''
+			// }
+		} catch (e) {
+			const statusCode = (e as ClientResponseError).status
+			if (statusCode === 404) {
+				error = 'Error: 404 - Username not found'
+			} else {
+				error = errorMessage(e)
+			}
+			// return fail(statusCode, {
+			// 	error: errorMessage(e)
+			// })
+		}
+
+		selectLoading = false
+		refocus()
+	}
 </script>
 
 {#if $modalStore[0]}
 	<div class="modal-example-form {cBase}">
 		<header class={cHeader}>{$modalStore[0].title ?? '(title missing)'}</header>
 		<article>{$modalStore[0].body ?? '(body missing)'}</article>
-		<form
+		<!-- <form
 			class="modal-form relative {cForm}"
 			method="POST"
 			action="?/selectUser"
@@ -60,7 +144,8 @@
 					refocus()
 				}
 			}}
-		>
+		> -->
+		<form>
 			<label class="label">
 				<span>Username</span>
 				<div class="flex gap-2">
@@ -71,12 +156,16 @@
 						bind:value
 						bind:this={inputRef}
 					/>
-					<button class="btn btn-md variant-filled rounded-md" disabled={selectLoading}>Add</button>
+					<button
+						class="btn btn-md variant-filled rounded-md"
+						disabled={selectLoading}
+						on:click={() => selectUser(value)}>Add</button
+					>
 				</div>
 			</label>
 			<FormError bind:error />
 		</form>
-		<form
+		<!-- <form
 			class="flex gap-1"
 			method="POST"
 			action="?/deselectUser"
@@ -94,7 +183,8 @@
 					refocus()
 				}
 			}}
-		>
+		> -->
+		<form>
 			{#if $modalStore[0].meta.currentUsername}
 				<div
 					class={`chip variant-filled rounded-full pointer-events-none text-black ${mainColor} shadow`}
