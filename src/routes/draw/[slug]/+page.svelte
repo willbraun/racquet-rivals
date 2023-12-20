87a85360b4e3ee146912c2e3896894d3a9b0889c
willbraun <williamhbraun1@gmail.com>
2023-12-19 21:53:22 -0500

    cleaned up some styling, added hover to draw header

--- src/routes/draw/[slug]/+page.svelte
+++ src/routes/draw/[slug]/+page.svelte
@@ -127,16 +127,10 @@
 		return [player1, player2]
 	}
 
-	let titleSelect: HTMLSelectElement
-	let headerButtons: HTMLElement
 	let roundHeader: HTMLElement
 	let drawGrid: HTMLElement
 	onMount(() => {
 		sessionStorage.setItem('loginGoto', location.pathname)
-
-		// titleSelect.style.width = `${titleSelect.offsetWidth + 20}px`
-		// console.log(headerButtons.style)
-		// headerButtons.style.marginLeft = `${parseInt(headerButtons.style.marginLeft) - 20}px`
 
 		if (roundHeader && drawGrid) {
 			roundHeader.addEventListener('scroll', () => {
@@ -168,7 +162,7 @@
 </script>
 
 <header class="flex items-center p-4 {headerColor}">
-	<a href="/">
+	<a class="hover:bg-primary-200 p-2 rounded" href="/">
 		<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 576 512"
 			><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path
 				d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
@@ -176,9 +170,8 @@
 		>
 	</a>
 	<select
-		class="select flex-grow ml-4 bg-transparent text-lg md:text-3xl font-bold border-none cursor-pointer whitespace-pre-wrap brightness-0 hover:brightness-0"
+		class="select flex-grow ml-2 mr-4 bg-transparent text-lg md:text-3xl font-bold border-none cursor-pointer whitespace-pre-wrap hover:bg-primary-200"
 		on:change={(e) => (drawUrl = e.currentTarget.value)}
-		bind:this={titleSelect}
 	>
 		<option disabled>Active Draws</option>
 		{#each data.active.items as draw}
@@ -194,8 +187,7 @@
 		{/each}
 	</select>
 	<div
-		class="w-fit ml-auto flex-none self-start flex flex-col justify-end items-end gap-2 flex-wrap"
-		bind:this={headerButtons}
+		class="w-fit ml-auto flex-none self-start flex flex-col sm:flex-row justify-end items-end gap-2 flex-wrap"
 	>
 		{#if $isAuth}
 			<Logout />
