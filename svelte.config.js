eecf990ffa506503a8e4796b42d2806605faac77
willbraun <williamhbraun1@gmail.com>
2023-12-20 08:55:22 -0500

    added adapter-netlify and toml

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
+import adapter from '@sveltejs/adapter-netlify'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [ vitePreprocess()],
-	
+	preprocess: [vitePreprocess()],
+
 	vitePlugin: {
-		inspector: true,
+		inspector: true
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -18,5 +17,5 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
-};
-export default config;
+}
+export default config
