7eb3e585f96462f6475f714dae6597e9ddf22923
willbraun <williamhbraun1@gmail.com>
2024-01-09 15:20:28 -0500

    updated how to play content

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is the frontend repo for [racquetrivals.com](https://www.racquetrivals.com)
 
 I am a huge tennis fan, and my wife and I like to create brackets for the final rounds of pro tournaments as a (somewhat) friendly competition, though it takes some time to set up each bracket by hand and make all your predictions.
 
-To streamline this, Racquet Rivals pulls in match results automatically and allows you to view current tournaments, pick who you think will win each round, earn points if you're correct, and compare with your friends. It's like March Madness for tennis tournaments.
+To streamline this, Racquet Rivals pulls in match results automatically and allows you to view current tournaments. Then pick who you think will win each round, earn points if you're correct, and compare with your friends. It's like March Madness for tennis tournaments.
 
 It only shows from the Round of 16 onwards to simplify the bracket-making process. Tennis tournaments can have up to 128 men and 128 women, and making picks for 254 matches per tournament is too much to deal with.
 
--- src/lib/HowToPlayContent.svelte
+++ src/lib/HowToPlayContent.svelte
@@ -14,8 +14,31 @@
 </script>
 
 {#if $modalStore[0]}
-	<div class="card p-4 w-modal shadow-xl space-y-4 bg-white">
+	<div class="card p-4 w-modal shadow-xl space-y-4 bg-white scroll-smooth">
 		<header class="text-2xl font-bold">{$modalStore[0].title ?? '(title missing)'}</header>
+		<article>
+			<p class="mb-4">Welcome to Racquet Rivals! Follow these steps to join the fun.</p>
+			<ol class="list-decimal px-8 [&>li]:mb-4">
+				<li>
+					Create an account. Choose a username, as well as an email address to receive
+					notifications.
+				</li>
+				<li>Select an active tournament and follow along.</li>
+				<li>
+					Once all players reach the Round of 16, you'll be notified via email and have 12 hours to
+					create your bracket for the tournament. You may enter predictions before this point for
+					any available players.
+				</li>
+				<li>As matches are completed, you'll win points if your predictions are correct.</li>
+				<ul class="list-disc ml-8 mb-4">
+					<li>Quarterfinals: 1 point</li>
+					<li>Semifinals: 2 points</li>
+					<li>Final: 4 points</li>
+					<li>Champion: 8 points</li>
+				</ul>
+				<li>Enter your friends' usernames in the Users menu to see how you stack up!</li>
+			</ol>
+		</article>
 		<footer class="modal-footer {parent.regionFooter}">
 			<button
 				class="btn rounded-md variant-glass-primary"
