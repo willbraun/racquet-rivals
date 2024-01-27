87478c50f3aa7ff2cf549871a95ade45b57efe03
willbraun <williamhbraun1@gmail.com>
2024-01-27 15:08:07 -0500

    take 2 at showing double last names in predictions

--- src/routes/draw/[slug]/ViewPrediction.svelte
+++ src/routes/draw/[slug]/ViewPrediction.svelte
@@ -5,7 +5,7 @@
 	export let getColor: (userId: string | undefined) => string
 
 	$: color = getColor(prediction.user_id)
-	$: display = prediction.name.split(' ').at(-1)
+	$: display = prediction.name.replace(') ', '').split(' ').slice(1).join(' ')
 </script>
 
 <div
