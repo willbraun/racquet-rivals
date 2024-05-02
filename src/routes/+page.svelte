06cf3417cb900db19c0a8617d1344a293172c7e7
willbraun <williamhbraun1@gmail.com>
2024-05-02 10:57:35 -0600

    updated french open start date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,7 +74,7 @@
 			<h3
 				class="text-2xl sm:text-4xl text-center rounded-xl p-4 text-white font-bold bg-orange-600 shadow-lg mb-8 mx-4"
 			>
-				Join us for the next major tournament - the French Open!<br /><br /> May 20, 2024 - June 9, 2024
+				Join us for the next major tournament - the French Open!<br /><br /> May 26, 2024 - June 9, 2024
 			</h3>
 			<h3 class="text-4xl sm:text-6xl text-center mb-8">Active Draws</h3>
 			{#if data.active.totalItems > 0}
