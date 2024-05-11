3f658e42e22c4bfd6b3c50f49e7f1e2f92cad3fe
willbraun <williamhbraun1@gmail.com>
2024-05-11 17:52:50 -0400

    added script to test all repos

---
+++ test-all.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+echo "Testing Racquet Rivals frontend..."
+npm test
+
+if [ $? -eq 0 ]; then
+    echo "Frontend tests passed ✅"
+else
+    echo "Frontend tests failed ❌"
+    exit 1
+fi
+
+echo "Testing Racquet Rivals backend..."
+cd ../tennis-bracket-pb-extend &&
+go test
+
+if [ $? -eq 0 ]; then
+    echo "Backend tests passed ✅"
+else
+    echo "Backend tests failed ❌"
+    exit 1
+fi
+
+echo "Testing Racquet Rivals web scraper..."
+cd ../tennis-bracket-scripts &&
+go test
+
+if [ $? -eq 0 ]; then
+    echo "Web scraping tests passed ✅"
+else
+    echo "Web scraping tests failed ❌"
+    exit 1
+fi
+
+echo "All tests passed ✅ ✅ ✅"
