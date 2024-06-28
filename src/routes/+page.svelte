337542ec250169c7ac9075bae9f148177fb2c791
willbraun <williamhbraun1@gmail.com>
2024-06-28 12:07:27 -0400

    updated Wimbledon message, added tailwind prettier plugin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,47 +24,47 @@
 	})
 </script>
 
-<header class="absolute top-0 right-0 flex justify-end items-center gap-2 p-4">
+<header class="absolute right-0 top-0 flex items-center justify-end gap-2 p-4">
 	<HowToPlay />
 	{#if $isAuth}
 		<Logout />
 	{/if}
 </header>
-<main class="w-full h-max p-4 bg-stone-100">
-	<div class="max-w-5xl mx-auto h-full">
+<main class="h-max w-full bg-stone-100 p-4">
+	<div class="mx-auto h-full max-w-5xl">
 		<section class="mb-16">
-			<div class="grid grid-cols-4 items-center my-16">
+			<div class="my-16 grid grid-cols-4 items-center">
 				<img src={bracketLeft} alt="left bracket" />
 				<h1
-					class="col-span-2 text-5xl sm:text-6xl md:text-8xl lg:text-9xl text-center font-bold tracking-tight whitespace-pre-line"
+					class="col-span-2 whitespace-pre-line text-center text-5xl font-bold tracking-tight sm:text-6xl md:text-8xl lg:text-9xl"
 				>
 					{'Racquet\nRivals'}
 				</h1>
 				<img src={bracketLeft} alt="right bracket" class="rotate-180" />
 			</div>
 			{#if $isAuth}
-				<div class="{pillStyle} shadow-lg col-start-1 col-end-2 bg-blue-300 w-fit mx-auto">
+				<div class="{pillStyle} col-start-1 col-end-2 mx-auto w-fit bg-blue-300 shadow-lg">
 					Welcome {data.pb_auth_username}!
 				</div>
 			{:else}
-				<div class="grid grid-rows-4 grid-cols-2 gap-4">
-					<div class="{pillStyle} shadow-xl col-start-1 col-end-3 sm:col-end-2 bg-blue-300">
+				<div class="grid grid-cols-2 grid-rows-4 gap-4">
+					<div class="{pillStyle} col-start-1 col-end-3 bg-blue-300 shadow-xl sm:col-end-2">
 						Create brackets for the last 16 players of pro tennis tournaments
 					</div>
 					<div
-						class="{pillStyle} shadow-xl row-start-2 col-start-1 sm:col-start-2 col-end-3 bg-orange-300"
+						class="{pillStyle} col-start-1 col-end-3 row-start-2 bg-orange-300 shadow-xl sm:col-start-2"
 					>
 						Earn points if your players win, and see how you stack up with your friends
 					</div>
 					<a
 						href="/login"
-						class="block {pillStyle} shadow-xl font-semibold row-start-3 col-start-1 col-end-3 sm:col-end-2 bg-yellow-200 hover:scale-105 duration-100"
+						class="block {pillStyle} col-start-1 col-end-3 row-start-3 bg-yellow-200 font-semibold shadow-xl duration-100 hover:scale-105 sm:col-end-2"
 					>
 						<button type="button">Login</button>
 					</a>
 					<a
 						href="/create-account"
-						class="block {pillStyle} shadow-xl font-semibold row-start-4 col-start-1 sm:col-start-2 col-end-3 bg-purple-300 hover:scale-105 duration-100"
+						class="block {pillStyle} col-start-1 col-end-3 row-start-4 bg-purple-300 font-semibold shadow-xl duration-100 hover:scale-105 sm:col-start-2"
 					>
 						<button type="button">Sign up</button>
 					</a>
@@ -72,17 +72,18 @@
 			{/if}
 		</section>
 		<section class="mb-24">
-			<h3
-				class="text-2xl sm:text-4xl text-center rounded-xl p-4 bg-gradient-to-r from-green-600 to-purple-600 text-white font-bold shadow-lg mb-8 mx-4"
+			<div
+				class="mx-4 mb-12 flex flex-col gap-8 rounded-xl bg-gradient-to-r from-green-600 to-purple-600 p-4 text-center text-2xl font-bold text-white shadow-lg sm:text-4xl"
 			>
-				Join us for the next major tournament - Wimbledon!<br /><br /> July 1, 2024 - July 14, 2024
-			</h3>
-			<h3 class="text-4xl sm:text-6xl text-center mb-8">Active Draws</h3>
+				<p>Wimbledon</p>
+				<p>July 1, 2024 - July 14, 2024</p>
+			</div>
+			<h3 class="mb-8 text-center text-4xl sm:text-6xl">Active Draws</h3>
 			{#if data.active.totalItems > 0}
 				{#each data.active.items as draw}
 					<a
 						href={`/draw/${getSlug(draw)}`}
-						class="{pillStyle} shadow-lg bg-green-300 p-4 mb-4 w-fit mx-auto hover:scale-105 duration-100"
+						class="{pillStyle} mx-auto mb-4 w-fit bg-green-300 p-4 shadow-lg duration-100 hover:scale-105"
 					>
 						<button type="button">
 							{getTitle(draw)}
@@ -90,14 +91,14 @@
 					</a>
 				{/each}
 			{:else}
-				<p class="text-center text-xl mb-16">No active draws</p>
+				<p class="mb-16 text-center text-xl">No active draws</p>
 			{/if}
-			<h3 class="text-4xl sm:text-6xl text-center mt-16 mb-8">Completed Draws</h3>
+			<h3 class="mb-8 mt-16 text-center text-4xl sm:text-6xl">Completed Draws</h3>
 			{#if data.completed.totalItems > 0}
 				{#each data.completed.items as draw}
 					<a
 						href={`/draw/${getSlug(draw)}`}
-						class="{pillStyle} shadow-lg bg-slate-300 p-4 mb-4 w-fit mx-auto hover:scale-105 duration-100"
+						class="{pillStyle} mx-auto mb-4 w-fit bg-slate-300 p-4 shadow-lg duration-100 hover:scale-105"
 					>
 						<button type="button">
 							{getTitle(draw)}
