f903557c56755db0a73330a2d842083c91b6ee64
willbraun <williamhbraun1@gmail.com>
2024-10-20 14:29:51 -0400

    added back button to profile page

--- src/routes/profile/[username]/+page.svelte
+++ src/routes/profile/[username]/+page.svelte
@@ -2,6 +2,7 @@
 	import { format } from 'date-fns'
 	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton'
 	import { DrawStatus, type ProfilePageData } from '$lib/types'
+	import arrowLeft from '$lib/images/icons/arrow-left-solid.svg'
 	import NavMenu from '$lib/components/NavMenu.svelte'
 	import HowToPlay from '$lib/components/HowToPlay.svelte'
 	import Rank from '$lib/components/Rank.svelte'
@@ -9,6 +10,14 @@
 	import InfoIcon from './InfoIcon.svelte'
 
 	export let data: ProfilePageData
+
+	const goBack = () => {
+		if (window.history.length > 1) {
+			window.history.back()
+		} else {
+			window.location.href = '/'
+		}
+	}
 
 	const formatRank = (num: number | null): string => {
 		if (num === null) {
@@ -57,14 +66,17 @@
 	}
 </script>
 
-<header class="flex items-center gap-4 p-4">
-	<a class="mr-auto px-2" href="/">
+<header class="flex items-center gap-4 px-4 py-[18px]">
+	<a class="px-2" href="/">
 		<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 576 512"
 			><!--! Home Icon - Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
 			<path
 				d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
 			/>
 		</svg>
+	</a>
+	<a href="/" on:click|preventDefault={goBack} class="mr-auto">
+		<img src={arrowLeft} alt="back" width="24" />
 	</a>
 	<HowToPlay />
 	<NavMenu />
@@ -160,7 +172,7 @@
 							<li
 								class={`grid w-full grid-cols-8 items-center gap-4 p-2 md:p-4 md:hover:brightness-105 ${
 									index % 2 ? 'bg-primary-50' : 'bg-primary-200'
-								} ${status === DrawStatus.ACTIVE && 'animate-pulse-green'} ${showCutoff ? 'border-pure-red border-b-2' : ''}`}
+								} ${status === DrawStatus.ACTIVE && 'animate-pulse-green'} ${showCutoff ? 'border-b-2 border-pure-red' : ''}`}
 							>
 								<p class="col-span-6 flex md:text-4xl">
 									{getTitle(drawResult)}
@@ -187,7 +199,7 @@
 							</li>
 							{#if showCutoff}
 								<div
-									class="bg-pure-red absolute left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 rounded px-2 text-xs font-bold text-white"
+									class="absolute left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 rounded bg-pure-red px-2 text-xs font-bold text-white"
 								>
 									OVERALL RANK CUTOFF
 								</div>
