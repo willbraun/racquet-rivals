62e2f79f8eb3d2b014e6bcd5631699f1cccd882f
willbraun <williamhbraun1@gmail.com>
2024-10-20 15:33:29 -0400

    changed most inline svgs to img imports

--- src/routes/draw/[slug]/AddPrediction.svelte
+++ src/routes/draw/[slug]/AddPrediction.svelte
@@ -3,6 +3,7 @@
 	import { popup } from '@skeletonlabs/skeleton'
 	import ViewPrediction from './ViewPrediction.svelte'
 	import type { Prediction, Slot, AddPredictionResult } from '$lib/types'
+	import plus from '$lib/images/icons/plus.svg'
 	import { makeSetType } from '$lib/utils'
 	import FormError from '$lib/components/FormError.svelte'
 	import { predictionStore } from '$lib/store'
@@ -54,12 +55,7 @@
 		<ViewPrediction {prediction} {getColor} />
 	{:else if predictionsAllowed}
 		<span class="text-xs">Add</span>
-		<svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 448 512">
-			<!--! Plus Icon - Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-			<path
-				d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
-			/>
-		</svg>
+		<img src={plus} alt="Add Prediction" width="12" />
 	{:else}
 		<span class="italic">None</span>
 	{/if}
--- src/routes/draw/[slug]/SelectUsers.svelte
+++ src/routes/draw/[slug]/SelectUsers.svelte
@@ -6,6 +6,7 @@
 	import { addUser, mainColor, makeSetType, removeUser } from '$lib/utils'
 	import { fade } from 'svelte/transition'
 	import { selectedUsers } from '$lib/store'
+	import x from '$lib/images/icons/x.svg'
 	export let parent
 
 	const modalStore = getModalStore()
@@ -104,12 +105,7 @@
 					transition:fade={{ duration: 100 }}
 				>
 					<p>{user.username}</p>
-					<svg xmlns="http://www.w3.org/2000/svg" height="1rem" viewBox="0 0 384 512">
-						<!--! X icon - Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-						<path
-							d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
-						/>
-					</svg>
+					<img src={x} alt="close" width="12" />
 				</button>
 			{/each}
 		</div>
