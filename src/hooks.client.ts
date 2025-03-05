44f15e22f8b2a02794e42f44f282ca7e72841e47
willbraun <williamhbraun1@gmail.com>
2025-03-05 09:22:26 -0500

    updates to pocketbase.ts

--- src/hooks.client.ts
+++
@@ -1,9 +0,0 @@
-import { pb } from '$lib/pocketbase'
-import { currentUser } from '$lib/store'
-import type { UserRecord } from '$lib/types'
-
-pb.authStore.loadFromCookie(document.cookie)
-pb.authStore.onChange(() => {
-	currentUser.set(pb.authStore.record as UserRecord)
-	document.cookie = pb.authStore.exportToCookie({ httpOnly: false })
-}, true)
--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,8 @@
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public'
 import Pocketbase from 'pocketbase'
 import Cookies from 'js-cookie'
+import { currentUser } from './store'
+import type { UserRecord } from './types'
 
 // Create a single PocketBase instance for the client
 export const pb = new Pocketbase(PUBLIC_POCKETBASE_URL)
@@ -13,3 +15,11 @@ if (typeof window !== 'undefined') {
 		pb.authStore.loadFromCookie(storedAuth)
 	}
 }
+
+// Update the currentUser store when the authStore changes
+pb.authStore.onChange(() => {
+	console.log('authStore.onChange')
+	currentUser.set(pb.authStore.record as UserRecord)
+	const auth = pb.authStore.exportToCookie({ httpOnly: false })
+	Cookies.set('pb_auth', auth, { secure: true, sameSite: 'strict' })
+}, true)
