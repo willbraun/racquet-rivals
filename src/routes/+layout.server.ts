81e3d07ab257526a0aa77fd34762f779641d1ffb
willbraun <williamhbraun1@gmail.com>
2025-10-06 08:32:02 -0400

    adding fallback for defaultDraw if no future draws

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,25 @@
-import type { Draw, PbListResponse, RootLayoutData } from '$lib/types'
-import { fetchJson } from '$lib/server/utils.js'
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public'
+import { fetchJson } from '$lib/server/utils.js'
+import type { Draw, PbListResponse, RootLayoutData } from '$lib/types'
 
 export async function load({ locals }) {
 	const url = PUBLIC_POCKETBASE_URL
 	const token = locals.pb.authStore.token
 
-	const draws: PbListResponse<Draw> = await fetchJson(
+	let draws: PbListResponse<Draw>
+	draws = await fetchJson(
 		`${url}/api/collections/draw/records?filter=(end_date>=@now)&sort=start_date,event&perPage=1`,
 		fetch,
 		token
 	)
+
+	if (draws.items.length === 0) {
+		draws = await fetchJson(
+			`${url}/api/collections/draw/records?sort=-end_date,event&perPage=1`,
+			fetch,
+			token
+		)
+	}
 
 	return {
 		cookieCurrentUser: locals.pb.authStore.record,
