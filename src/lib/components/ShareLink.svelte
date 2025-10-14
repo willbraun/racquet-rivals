306522b5060226ebffaa20dae1f7745c780681bf
willbraun <williamhbraun1@gmail.com>
2025-10-14 15:57:19 -0400

    removed debug logic

--- src/lib/components/ShareLink.svelte
+++ src/lib/components/ShareLink.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import share from '$lib/images/icons/share-from-square-solid.svg'
-	import { navMenuOpen, shareLinkOpen } from '$lib/store'
+	import { isMobile, navMenuOpen, shareLinkOpen } from '$lib/store'
 </script>
 
-<!-- {#if $isMobile} -->
-<button
-	onclick={() => {
-		navMenuOpen.set(false)
-		setTimeout(() => shareLinkOpen.set(true), 250)
-	}}
-	class="grid w-full grid-cols-4 items-center gap-4"
-	data-testid="share-link-icon"
->
-	<img src={share} alt="share" width="24" class="justify-self-center" />
-	<p class="col-span-3 justify-self-start text-xl">Share</p>
-</button>
-<!-- {/if} -->
+{#if $isMobile}
+	<button
+		onclick={() => {
+			navMenuOpen.set(false)
+			setTimeout(() => shareLinkOpen.set(true), 250)
+		}}
+		class="grid w-full grid-cols-4 items-center gap-4"
+		data-testid="share-link-icon"
+	>
+		<img src={share} alt="share" width="24" class="justify-self-center" />
+		<p class="col-span-3 justify-self-start text-xl">Share</p>
+	</button>
+{/if}
